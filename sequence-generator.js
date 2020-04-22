8867fd8618a6a5163c66c7f5ce97a25ee2662b47
Steve Occhipinti <dev@stevenocchipinti.com>
2020-04-22 17:59:43 +1000

    Updates

---
+++ sequence-generator.js
@@ -0,0 +1,73 @@
+/**
+ * A function intended to be called every second in a setInterval
+ */
+function* sequenceGenerator({
+  exerciseSeconds,
+  exerciseRestSeconds,
+  rounds,
+  roundRestSeconds,
+  exercises: exercisesRaw,
+}) {
+  let secondsElapsed = 0
+  let round = 0
+
+  const exercises = exercisesRaw
+    .split("\n")
+    .map((s) => s.trim())
+    .filter(Boolean)
+
+  while (round < rounds) {
+    for (exercise of exercises) {
+      // exercise
+      while (secondsElapsed < exerciseSeconds) {
+        yield {
+          exercise,
+          totalSeconds: exerciseSeconds,
+          secondsRemaining: exerciseSeconds - secondsElapsed,
+        }
+        secondsElapsed++
+      }
+      secondsElapsed = 0
+
+      // exercise rest
+      if (exercise !== exercises[exercise.length - 1]) {
+        while (secondsElapsed < exerciseRestSeconds) {
+          yield {
+            exercise: "rest",
+            totalSeconds: exerciseRestSeconds,
+            secondsRemaining: exerciseRestSeconds - secondsElapsed,
+          }
+          secondsElapsed++
+        }
+        secondsElapsed = 0
+      }
+    }
+
+    round++
+
+    // round rest
+    if (round < rounds) {
+      while (secondsElapsed < roundRestSeconds) {
+        yield {
+          exercise: "rest",
+          totalSeconds: roundRestSeconds,
+          secondsRemaining: roundRestSeconds - secondsElapsed,
+        }
+        secondsElapsed++
+      }
+      secondsElapsed = 0
+    }
+  }
+}
+
+// const s = sequenceGenerator({
+//   exerciseSeconds: 2,
+//   exerciseRestSeconds: 3,
+//   rounds: 3,
+//   roundRestSeconds: 5,
+//   exercises: "foo",
+// })
+
+// setInterval(() => console.log(s.next().value), 1000)
+
+module.exports = sequenceGenerator
---
+++ sequence-generator.test.js
@@ -0,0 +1,103 @@
+const sequenceGenerator = require("./sequence-generator")
+
+describe("sequence", () => {
+  it("works", () => {
+    const s = sequenceGenerator({
+      exerciseSeconds: 10,
+      exerciseRestSeconds: 3,
+      rounds: 2,
+      roundRestSeconds: 5,
+      exercises: "foo\n   bar\n baz   ",
+    })
+
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 5, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 5, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 5, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 5, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 5, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "foo", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "bar", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "rest", totalSeconds: 3, secondsRemaining: 1 })
+
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 10 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 9 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 8 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 7 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 6 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 5 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 4 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 3 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 2 })
+    expect(s.next().value).toEqual({ exercise: "baz", totalSeconds: 10, secondsRemaining: 1 })
+
+    expect(s.next().done).toEqual(true)
+  })
+})
